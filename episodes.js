3e9ce2f3e58ca1cd7b58938bdc517055c87df88f
Henry <henry@reed.edu>
2018-10-12 20:51:34 -0700

    added audio-playback capabilities

--- episodes.js
+++ episodes.js
@@ -1,37 +1,39 @@
 var episodes = [
    {
-      "name":"Religion [More Specifics to Come]",
-      "desc":"A conversation with Naomi Fastovsky. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "name":"What is Religion?",
+      "desc":"A dive into seemingly straightforward question with a religious studies major at Reed College. The conversation starts off by considering the percieved differences between religions in Europe and religions in eastern Asia. However though trying to refine this question, we develop a much larger perspective about religions in general, and also how such a simplistic comparison doesn't yield what truly makes religions interesting.",
+      "link":"https://medium.com/@henry_4862/religion-the-eurasian-east-and-west-d49b35db0c34",
       "src":""
    },
    {
       "name":"Physiology at Berkeley: A case study of the Undergraduate Medical Student",
-      "desc":"A conversation with a student at the University of California, Berkeley. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "desc":"A conversation with a student at the University of California, Berkeley. First, we talk about the pre-med pathway through undergraduate education, complete with all of its intensities. Then we explore the differences in college life between the large public University of California Berkeley and a small private Reed College.",
       "src":""
    },
    {
       "name":"Immigration Policy for the United States",
-      "desc":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "desc":"Yuta and Henry duel it out over immigration policy for the US. On one side is the disconcerted nationalist, supporting larger restrictions on immigration and priority for the recognized citizens of a country when push comes to shove. This side also considers the culture of one's country inherently valuable and worth protecting politically. On the other side is the more economically-focussed perspective that sees the overwhelming academic consensus that freer markets and freer immigration leads to a better quality of life for everyone involved, in the long term.",
       "src":""
    },
    {
       "name":"Social Media - the Good, the Bad, and Twitter",
-      "desc":"A conversation with Kayleigh Bangs. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "desc":"Social Media in many forms has come to dominate human-to-human interactions. In a prompt, Henry divides the phenomenon into two categories: Direct and Indirect. We have on a guest to help us point out the flaws in social media, our personal gribes with it, but also its innovative features.",
+      "link":"https://medium.com/@henry_4862/the-role-of-social-media-43510585c6cc",
       "src":""
    },
    {
       "name":"The Art of Video Games",
-      "desc":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "desc":"Yuta and Henry duel it out over what types of video games are truly <i>art</i>. Perhaps the aspects of video games that resemble more-entrenched forms of art like novels, movies, and the visual arts make the new medium worthy of <i>art</i>, but it brings nothing fundamentally new to the table. Or perhaps the interaction and game-mechanical aspects of video games are actually a new dimension to artistic expression not derivative of the more-entrenched art forms.",
       "src":""
    },
    {
       "name":"Mental Illness in the United States",
-      "desc":"A conversation with Carolyn Walworth and John Petrenovich. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      "src":""
+      "desc":"A discussion about recent history and current developments in the mental health scene, with two guest undergraduate psychology majors. Why and how are the suicide rates increasing? How effective are psychoactive medications and what do we know about them? How do we view mental health and its treatment in our society?",
+      "src":"episodes/09-01-18_Mental-Illness.mp3"
    },
    {
       "name":"The Significance of Liberal Arts Education",
-      "desc":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "desc":"We both go to a liberal arts school ourselves, but each one is unique. What is the fundamental, standard foundation for a libear arts education, and why do we consider it important? Before this episode, we listened to a discussion along similar lines held on the <i>Partially Examined Life</i> podcast.",
       "src":""
    },
    {
@@ -56,7 +58,8 @@ var episodes = [
    },
    {
       "name":"Appealing to the Public",
-      "desc":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+      "desc":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "src":"episodes/09-01-18_Mental-Illness.mp3"
    },
    {
       "name":"Jordan Peterson",
--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+/************************************
+ * Episodes Data
+ ************************************/
+
 var episodes_container = document.getElementById("episodes-container")
+
+/************************************
+ * Generate HTML
+ ************************************/
 
 function make_episode(ep) {
     // ep has name, desc, and src
@@ -6,35 +14,132 @@ function make_episode(ep) {
     episode.classList.add("episode")
     
     let name = document.createElement("div")
+    episode.appendChild(name)
     name.classList.add("name")
     name.innerText = ep["name"]
-    episode.appendChild(name)
 
     let index = document.createElement("div")
+    episode.appendChild(index)
     index.classList.add("index")
     index.innerText = "ep:" + (episodes.length - i).toString()
-    episode.appendChild(index)
     
     let desc = document.createElement("div")
+    episode.appendChild(desc)
     desc.classList.add("desc")
     desc.innerHTML = ep["desc"]
-    episode.appendChild(desc)
 
-    let player = document.createElement("div")
-    player.classList.add("player")
-    episode.appendChild(player)
+    if (ep.hasOwnProperty("link")) {
+        let link = document.createElement("a")
+        episode.appendChild(link)
+        link.classList.add("link")
+        link.innerText = "Read More"
+        link.href = ep["link"]
+    }
 
-    let player_btn = document.createElement("img")
-    player_btn.classList.add("btn")
-    player_btn.src = "assets/images/btn_play.png"
-    player.appendChild(player_btn)
+    // PLAYER
 
-    let player_bar = document.createElement("div")
-    player_bar.classList.add("bar")
-    player.appendChild(player_bar)
+    let audio_player = document.createElement("div")
+    audio_player.classList.add("audio-player")
+    episode.appendChild(audio_player)
+    audio_player.innerHTML = `
+        <button class="audio-play"><i class="fa fa-play"></i><span>Play</span></button>
+        <button class="audio-pause"><i class="fa fa-pause"></i><span>Pause</span></button>
+        <div class="audio-progress-container">
+            <span class="audio-currenttime audio-time">00:00:00</span>
+            <progress class="audio-progress" value="0"></progress>
+            <span class="audio-duration audio-time">00:00:00</span>
+        </div>
+        <button class="audio-speed">1x</button>
+        <button class="audio-rewind"><i class="fa fa-fast-backward"></i><span>Rewind</span></button>`
+
+    let audio_source = document.createElement("audio")
+    audio_player.appendChild(audio_source)
+    audio_source.src = ep.src
+
+    connect_audio(audio_player)
 
     return episode
 }
+
+/************************************
+ * Connect Audio
+ ************************************/
+
+var speeds = [ 1, 1.5, 2, 2.5, 3 ]
+
+function connect_audio(audio_player) {
+    let player = audio_player
+
+    var audio = player.querySelector('audio');
+    var play = player.querySelector('.audio-play');
+    var pause = player.querySelector('.audio-pause');
+    var rewind = player.querySelector('.audio-rewind');
+    var progress = player.querySelector('.audio-progress');
+    var speed = player.querySelector('.audio-speed');
+    var currentTime = player.querySelector('.audio-currenttime');
+    var duration = player.querySelector('.audio-duration');
+
+    var currentSpeedIdx = 0;
+
+    pause.style.display = 'none';
+
+    var toHHMMSS = function ( totalsecs ) {
+        var sec_num = parseInt(totalsecs, 10); // don't forget the second param
+        var hours   = Math.floor(sec_num / 3600);
+        var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+        var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+        if (hours   < 10) {hours   = "0"+hours; }
+        if (minutes < 10) {minutes = "0"+minutes;}
+        if (seconds < 10) {seconds = "0"+seconds;}
+
+        var time = hours+':'+minutes+':'+seconds;
+        return time;
+    }
+
+    audio.addEventListener('loadedmetadata', function(){
+        progress.setAttribute('max', Math.floor(audio.duration));
+        duration.textContent  = toHHMMSS(audio.duration);
+    });
+
+    audio.addEventListener('timeupdate', function(){
+        progress.setAttribute('value', audio.currentTime);
+        currentTime.textContent  = toHHMMSS(audio.currentTime);
+    });
+
+    play.addEventListener('click', function(){
+        this.style.display = 'none';
+        pause.style.display = 'inline-block';
+        pause.focus();
+        audio.play();
+    }, false);
+
+    pause.addEventListener('click', function(){
+        this.style.display = 'none';
+        play.style.display = 'inline-block';
+        play.focus();
+        audio.pause();
+    }, false);
+
+    rewind.addEventListener('click', function(){
+        audio.currentTime -= 30;
+    }, false);
+
+    progress.addEventListener('click', function(e){
+    audio.currentTime = Math.floor(audio.duration) * (e.offsetX / e.target.offsetWidth);
+    }, false);
+
+    speed.addEventListener('click', function(){
+        currentSpeedIdx = currentSpeedIdx + 1 < speeds.length ? currentSpeedIdx + 1 : 0;
+        audio.playbackRate = speeds[currentSpeedIdx];
+        this.textContent  = speeds[currentSpeedIdx] + 'x';
+        return true;
+    }, false);
+}
+
+/************************************
+ * Run
+ ************************************/
 
 for (var i = 0; i < episodes.length; i++) {
     let ep = episodes[i]
